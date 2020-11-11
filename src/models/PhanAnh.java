9ea9d70dffbff74a7f16e2583d3403ce8a55663f
Hoang Trung Hieu <hieuit1142@gmail.com>
2020-11-11 22:52:06 +0700

    change 11/11/2020

--- src/models/PhanAnh.java
+++ src/models/PhanAnh.java
@@ -5,54 +5,46 @@
  */
 package models;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.sql.Date;
 
 /**
  *
  * @author Hoang Trung Hieu
  */
 public class PhanAnh {
-    Connection conn = MySQLConnection.getMySQLConnection();
+    private int id; // ma phan anh
     private String name; // ten nguoi phan anh
     private String content; // noi dung phan anh
-    private String date; // gnay phan anh
+    private Date date; // ngay phan anh
     private String type; // phan loai
     private String state; // trang thai
 
     public PhanAnh() {
     }
 
-    public PhanAnh(String name, String content, String date, String type, String state) {
+    public PhanAnh(int id, String name, String content, Date date, String type, String state) {
+        this.id = id;
         this.name = name;
         this.content = content;
         this.date = date;
         this.type = type;
         this.state = state;
     }
-    
-    /**
-     * @author Hoang Trung Hieu
-     */
-    public void insertPA() {
-        String sqlInsert = "INSERT INTO phan_anh (NGUOI_PHAN_ANH, NOI_DUNG, NGAY_PHAN_ANH, PHAN_LOAI, TRANG_THAI) "
-                            + "VALUES (?, ?, ?, ?, ?)";
-        
-        try {
-            PreparedStatement stmt = conn.prepareStatement(sqlInsert);
-            stmt.setString(1, name);
-            stmt.setString(2, content);
-            stmt.setString(3, date);
-            stmt.setString(4, type);
-            stmt.setString(5, state);
-            stmt.execute();
-        } catch (SQLException ex) {
-            Logger.getLogger(PhanAnh.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
+
+    public PhanAnh(String name, String content, Date date, String type, String state) {
+        this.name = name;
+        this.content = content;
+        this.date = date;
+        this.type = type;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -71,11 +63,11 @@ public class PhanAnh {
         this.content = content;
     }
 
-    public String getDate() {
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 
---
+++ src/models/PhanAnhModify.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author THINKBOOK
+ */
+public class PhanAnhModify {
+    
+    public static void insert(PhanAnh pa) {
+        Connection conn = null;
+        conn = MySQLConnection.getMySQLConnection();
+        
+        String sql = "INSERT INTO phan_anh (NGUOI_PHAN_ANH, NOI_DUNG, NGAY_PHAN_ANH, PHAN_LOAI, TRANG_THAI) "
+                            + "VALUES (?, ?, ?, ?, ?)";
+        PreparedStatement stmt = null;
+        try {
+            stmt = conn.prepareStatement(sql);
+            
+            stmt.setString(1, pa.getName());
+            stmt.setString(2, pa.getContent());
+            stmt.setDate(3, pa.getDate());
+            stmt.setString(4, pa.getType());
+            stmt.setString(5, pa.getState());
+            
+            stmt.execute();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(PhanAnhModify.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PhanAnhModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(PhanAnhModify.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+    }
+}
